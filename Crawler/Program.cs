1567694dde7d4935f6d2d040d033b010607cc832
Oakley <Oakley>
2017-11-28 14:09:14 +0800

    用stream 讀取大檔案 並且輸出 txt 檔

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -16,36 +16,29 @@ namespace Crawler
 
             var filePath = AppDomain.CurrentDomain.BaseDirectory + "11281132.html";
             //指定來源網頁
-            WebClient url = new WebClient();
-
-            var fs = new FileStream(filePath,FileMode.Open);
-            var result = "";
-            int iChar;
-
-
-            StreamReader srReader = new StreamReader(fs);
-            iChar = srReader.Read();
-            while (iChar != -1)
+            //WebClient url = new WebClient();
+            //var fs = new FileStream(filePath,FileMode.Open);
+            var htmlText = "";
+            //這方法開大檔案很快
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
             {
-                result += (Convert.ToChar(iChar));
-                iChar = srReader.Read();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    htmlText += line;
+                }
             }
-            srReader.Close();
-
-
             //Console.WriteLine(result);
             // MemoryStream ms = new MemoryStream(url.DownloadData("http://www.yahoo.com.tw"));
-       
 
             // 使用預設編碼讀入 HTML 
             HtmlDocument doc = new HtmlDocument();
             //doc.Load(fs, Encoding.Default);
-            doc.LoadHtml(result);
 
-            var result2 = doc.DocumentNode.Descendants()
-            .Where(x => x.Attributes.Contains("class") && x.Name =="h5").Select(x =>x.InnerText).ToList();
-           
-
+            doc.LoadHtml(htmlText);
+            var count = 1;
             var sharePeople = doc.DocumentNode
                 .Descendants("h5")
                 .Where(x => x.HasClass("_5pbw") && x.HasClass("_5vra"))
@@ -53,13 +46,22 @@ namespace Crawler
                 .Select(x=>x.FirstChild)
                 .Select(x=>x.FirstChild)
                 .Select(x=>x.FirstChild)
-                .Select(x=>x.InnerText).ToList();
+                .Select(x=>x.InnerText).Distinct().ToList();
 
-            foreach (var item in sharePeople)
-            {
-                Console.WriteLine(item);
+            using (StreamWriter sw = new StreamWriter("SharePeople.txt"))
+            {             
+          
+                foreach (var personName in sharePeople)
+                {
+                    Console.WriteLine($"{count}.{personName}");
+                    sw.WriteLine($"{count}.{personName}");
+                    //sw.WriteLine(Environment.NewLine);
+                    count++;
+                }
             }
+
             Console.WriteLine($"公開 分享總人數 : {sharePeople.Count}");
+
             // 裝載第一層查詢結果 
             HtmlDocument hdc = new HtmlDocument();
 
